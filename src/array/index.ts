b3d120b51706cdd04dd36564ea1759b7185618f8
yushan <yushanwebdev@gmail.com>
2022-04-13 18:18:29 +0530

    feat: Collecting hooks under index

---
+++ src/array/index.ts
@@ -0,0 +1,4 @@
+export { useBoolean } from './useBoolean';
+export type { UseBoolean, UseBooleanActions } from './useBoolean';
+export { useMap } from './useMap';
+export type { UseMap, MapOrEntries, UseMapActions } from './useMap';
