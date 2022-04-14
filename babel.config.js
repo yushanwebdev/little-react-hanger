c5425b5d1cf8c06eb11d00ca26932148f88f2d26
yushan <yushanwebdev@gmail.com>
2022-04-14 17:42:30 +0530

    feat: Implementing build step with Babel

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+const isCjs = process.env.IS_CJS === 'true';
+process.env.NODE_ENV = 'production';
+
+module.exports = {
+  presets: [
+    [
+      require('babel-preset-react-app'),
+      {
+        useESModules: !isCjs,
+        typescript: true,
+        flow: false,
+        helpers: true,
+        absoluteRuntime: false,
+      },
+    ],
+  ],
+  ignore: [
+    '**/src/**/*.d.ts',
+    '**/src/setupTests.js',
+    '**/src/setupTests.ts',
+    '**/src/**/__tests__/**/*.ts',
+    '**/src/**/__tests__/**/*.tsx',
+    '**/src/**/*.test.tsx',
+    '**/src/**/*.test.ts',
+  ],
+};
